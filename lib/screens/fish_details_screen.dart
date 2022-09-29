f17ed0f68dbb73e5c91874796da856a64106f29b
John Lester Necesito <52057825+Zacharias02@users.noreply.github.com>
2022-09-29 15:45:51 +0800

    init starter

--- lib/screens/fish_details_screen.dart
+++ lib/screens/fish_details_screen.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lns_aquarium/models/fish_model.dart';
 import 'package:lns_aquarium/resources/app_colors.dart';
-import 'package:lns_aquarium/widgets/fish_availability_widget.dart';
-import 'package:lns_aquarium/widgets/fish_details_header_widget.dart';
-import 'package:lns_aquarium/widgets/fish_details_location_widget.dart';
-import 'package:lns_aquarium/widgets/fish_details_phrases_widget.dart';
-import 'package:lns_aquarium/widgets/fish_details_title_widget.dart';
 
 class FishDetailsScreen extends StatelessWidget {
   const FishDetailsScreen({
@@ -15,40 +10,15 @@ class FishDetailsScreen extends StatelessWidget {
 
   final Fish fish;
 
-  final LinearGradient linearGradient = const LinearGradient(
-    begin: FractionalOffset.topCenter,
-    end: FractionalOffset.bottomCenter,
-    colors: <Color>[
-      AppColors.kPrimarySubtleColor,
-      AppColors.kBackgroundColor,
-    ],
-  );
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    // TODO: Implement your details screen.
+    return const Scaffold(
       backgroundColor: AppColors.kBackgroundColor,
-      body: SingleChildScrollView(
-        child: Container(
-          decoration: BoxDecoration(gradient: linearGradient),
-          child: Column(
-            children: <Widget>[
-              FishDetailsHeaderWidget(fish: fish),
-              FishDetailsTitleWidget(
-                title: fish.name,
-                rarity: fish.rarity,
-              ),
-              FishDetailsAvailabilityWidget(fish: fish),
-              FishDetailsPriceLocationWidget(
-                price: fish.price,
-                location: fish.location,
-              ),
-              FishDetailsPhrasesWidget(
-                catchPhrases: fish.catchPhrase,
-                museumPhrases: fish.museumPhrase,
-              ),
-            ],
-          ),
+      body: Center(
+        child: Text(
+          'Nothing to do here.',
+          style: TextStyle(fontSize: 20),
         ),
       ),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:lns_aquarium/models/fish_model.dart';
-import 'package:lns_aquarium/repositories/fishes_repository.dart';
-import 'package:lns_aquarium/widgets/fish_row_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,39 +8,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late FishesRepository repository;
-
-  AllFish? allFish;
-
-  Future<void> _fetchAllFish() async {
-    allFish = await repository.getFishes();
-
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    repository = FishesRepository();
-    _fetchAllFish();
-
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
+    // TODO: Implement your listing screen.
     return Scaffold(
       appBar: AppBar(title: const Text('LNS Aquarium')),
-      body: (allFish != null)
-          ? ListView.builder(
-              itemBuilder: _fishListViewBuilder,
-              itemCount: allFish!.getList.length,
-            )
-          : const Center(child: CircularProgressIndicator()),
+      body: const Center(
+        child: Text(
+          'Nothing to do here.',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
     );
-  }
-
-  Widget _fishListViewBuilder(BuildContext context, int index) {
-    Fish fish = allFish!.getList[index];
-    return FishRowWidget(fish: fish);
   }
 }
